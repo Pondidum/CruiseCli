98ec005500ab0c036a828b39bb4faad99fdcc643
andy <andy@andydote.co.uk>
2014-06-23 20:50:30 +0100

    cruise: initial version, add fubucore package ref

---
+++ Cruise/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using FubuCore.CommandLine;
+
+namespace Cruise
+{
+	class Program
+	{
+		static int Main(string[] args)
+		{
+			try
+			{
+				var factory = new CommandFactory();
+				factory.RegisterCommands(typeof(IFubuCommand).Assembly);
+				factory.RegisterCommands(typeof(Program).Assembly);
+
+				var executor = new CommandExecutor(factory);
+				var success = executor.Execute(args);
+
+				return success ? 0 : 1;
+			}
+			catch (CommandFailureException e)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("ERROR: " + e.Message);
+				Console.ResetColor();
+				return 1;
+			}
+			catch (Exception ex)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("ERROR: " + ex);
+				Console.ResetColor();
+				return 1;
+			}
+		}
+	}
+}
