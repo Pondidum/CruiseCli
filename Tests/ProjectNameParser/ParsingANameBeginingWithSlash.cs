b699df89c982f3dd8fcde270f08f21b13d70f32b
andy <andy@andydote.co.uk>
2014-06-24 20:35:12 +0100

    tests: add more projectNameParser tests

---
+++ Tests/ProjectNameParser/ParsingANameBeginingWithSlash.cs
@@ -0,0 +1,25 @@
+﻿using Should;
+using Xunit;
+
+namespace Tests.ProjectNameParser
+{
+	public class ParsingANameBeginingWithSlash : ProjectNameParserTestBase
+	{
+		public ParsingANameBeginingWithSlash()
+		{
+			Execute("/development");
+		}
+
+		[Fact]
+		public void Server_is_empty()
+		{
+			Result.Server.ShouldBeEmpty();
+		}
+
+		[Fact]
+		public void Project_is_development()
+		{
+			Result.Project.ShouldEqual("development");
+		}
+	}
+}
---
+++ Tests/ProjectNameParser/ParsingANameEndingWithSlash.cs
@@ -0,0 +1,25 @@
+﻿using Should;
+using Xunit;
+
+namespace Tests.ProjectNameParser
+{
+	public class ParsingANameEndingWithSlash : ProjectNameParserTestBase
+	{
+		public ParsingANameEndingWithSlash()
+		{
+			Execute("primary/");
+		}
+
+		[Fact]
+		public void Server_is_primary()
+		{
+			Result.Server.ShouldEqual("primary");
+		}
+
+		[Fact]
+		public void Project_is_blank()
+		{
+			Result.Project.ShouldBeEmpty();
+		}
+	}
+}
