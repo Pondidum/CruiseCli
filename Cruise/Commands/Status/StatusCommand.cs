db2d3b62c0abd1ebc43603f96d1ede43a5547a55
andy <andy@andydote.co.uk>
2014-06-25 19:36:42 +0100

    statusCommand: list usages, fixes usage tests

--- Cruise/Commands/Status/StatusCommand.cs
+++ Cruise/Commands/Status/StatusCommand.cs
@@ -4,6 +4,16 @@ namespace Cruise.Commands.Status
 {
 	public class StatusCommand : FubuCommand<StatusInputModel>
 	{
+		public StatusCommand()
+		{
+			Usage("Lists the status of all projects on all servers")
+				.Arguments()
+				.ValidFlags();
+
+			Usage("Lists the status for the given server/project")
+				.Arguments(a => a.Project);
+		}
+
 		public override bool Execute(StatusInputModel input)
 		{
 			return true;
