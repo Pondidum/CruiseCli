003780bfbfbec89cadf5442a129eef347aa4953d
andy <andy@andydote.co.uk>
2014-06-26 21:10:40 +0100

    storageController: stub methods out

---
+++ Cruise/Infrastructure/IFileSystem.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+
+namespace Cruise.Infrastructure
+{
+	public interface IFileSystem
+	{
+		void WriteFile(string path, Stream contents);
+		Stream ReadFile(string path);
+	}
+}
---
+++ Cruise/Storage/StorageController.cs
@@ -0,0 +1,28 @@
+﻿using Cruise.Infrastructure;
+
+namespace Cruise.Storage
+{
+	public class StorageController
+	{
+		private readonly IFileSystem _fileSystem;
+
+		public StorageController(IFileSystem fileSystem)
+		{
+			_fileSystem = fileSystem;
+		}
+
+		public StorageModel Load()
+		{
+			//using (var stream = _fileSystem.ReadFile(""))
+			//{
+			//	return JsonConvert.ToObject<StorageModel>(stream);
+			//}
+			return new StorageModel();
+		}
+
+		public void Save(StorageModel model)
+		{
+			//_fileSystem.WriteFile("", JsonConvert.ToJson(model));
+		}
+	}
+}
