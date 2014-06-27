3ca63b032d8cd16fe96b01d201c513824840d071
andy <andy@andydote.co.uk>
2014-06-27 20:09:01 +0100

    storageController: add json.net reference and implement Load and Save

--- Cruise/Storage/StorageController.cs
+++ Cruise/Storage/StorageController.cs
@@ -1,28 +1,45 @@
-﻿using Cruise.Infrastructure;
+﻿using System.IO;
+using Cruise.Infrastructure;
+using Newtonsoft.Json;
 
 namespace Cruise.Storage
 {
 	public class StorageController
 	{
+		private const string Filename = "Cruise.config";
+
 		private readonly IFileSystem _fileSystem;
+		private readonly string _path;
 
 		public StorageController(IFileSystem fileSystem)
 		{
 			_fileSystem = fileSystem;
+			_path = Path.Combine(_fileSystem.HomePath, Filename);
 		}
 
 		public StorageModel Load()
 		{
-			//using (var stream = _fileSystem.ReadFile(""))
-			//{
-			//	return JsonConvert.ToObject<StorageModel>(stream);
-			//}
-			return new StorageModel();
+			using (var stream = _fileSystem.ReadFile(_path))
+			{
+				using (var sr = new StreamReader(stream))
+				{
+					return JsonConvert.DeserializeObject<StorageModel>(sr.ReadToEnd());
+				}
+			}
 		}
 
 		public void Save(StorageModel model)
 		{
-			//_fileSystem.WriteFile("", JsonConvert.ToJson(model));
+			var json = JsonConvert.SerializeObject(model);
+
+			using (var ms = new MemoryStream())
+			using (var writer = new StreamWriter(ms))
+			{
+				writer.Write(json);
+				ms.Position = 0;
+
+				_fileSystem.WriteFile(_path, ms);
+			}
 		}
 	}
 }
