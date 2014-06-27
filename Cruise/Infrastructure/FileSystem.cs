b5a835102494d4c7f2f8a9394804aec2063adeb2
andy <andy@andydote.co.uk>
2014-06-27 17:05:36 +0100

    filesystem: add IFileSystem implementation

---
+++ Cruise/Infrastructure/FileSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace Cruise.Infrastructure
+{
+	public class FileSystem : IFileSystem
+	{
+		public void WriteFile(string path, Stream contents)
+		{
+			using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write))
+			{
+				contents.CopyTo(fs);
+			}
+		}
+
+		public Stream ReadFile(string path)
+		{
+			return new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read);
+		}
+	}
+}
