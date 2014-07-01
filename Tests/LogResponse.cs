808afa7945577c41b425765a01ff089c6395baf6
andy <andy@andydote.co.uk>
2014-07-01 20:05:17 +0100

    tests: add a logging IResponse implementation

---
+++ Tests/LogResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Cruise.Infrastructure;
+
+namespace Tests
+{
+	public class LogResponse : IResponse
+	{
+		public List<string> Log { get; private set; }
+
+		public LogResponse()
+		{
+			Log = new List<string>();
+		}
+
+		public void Write(string format, params object[] args)
+		{
+			Log.Add(string.Format(format, args));
+		}
+	}
+}
