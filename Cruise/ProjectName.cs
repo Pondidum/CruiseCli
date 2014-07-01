2f7c7dab0078b8f1a6b7974a60052efc79d8653d
andy <andy@andydote.co.uk>
2014-07-01 19:14:13 +0100

    projectName: add IsBlank property

--- Cruise/ProjectName.cs
+++ Cruise/ProjectName.cs
@@ -5,10 +5,14 @@
 		public string Server { get; private set; }
 		public string Project { get; private set; }
 
+		public bool IsBlank { get; private set; }
+
 		public ProjectName(string server, string project)
 		{
 			Server = server;
 			Project = project;
+
+			IsBlank = string.IsNullOrWhiteSpace(Server) & string.IsNullOrWhiteSpace(Project);
 		}
 	}
 }
