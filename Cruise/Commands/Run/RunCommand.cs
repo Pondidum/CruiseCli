265166f39d36a0a9a03a0efd9eda87181737a0cf
andy <andy@andydote.co.uk>
2014-07-04 19:02:32 +0100

    runCommand: add tests, adjust constructor

--- Cruise/Commands/Run/RunCommand.cs
+++ Cruise/Commands/Run/RunCommand.cs
@@ -1,10 +1,12 @@
-﻿using FubuCore.CommandLine;
+﻿using Cruise.Infrastructure;
+using Cruise.Transport;
+using FubuCore.CommandLine;
 
 namespace Cruise.Commands.Run
 {
 	public class RunCommand : FubuCommand<RunInputModel>
 	{
-		public RunCommand()
+		public RunCommand(IResponse writer, ITransportModel transport)
 		{
 			Usage("Forces a build for the given server/project")
 				.Arguments(a => a.Project);
---
+++ Tests/Commands/RunCommandTests/RunTests.cs
@@ -0,0 +1,115 @@
+using System.Linq;
+using Cruise.Commands.Run;
+using Cruise.Transport;
+using NSubstitute;
+using Should;
+using Xunit;
+
+namespace Tests.Commands.RunCommandTests
+{
+	public class RunTests : CommandTestBase
+	{
+		private readonly ITransportModel _transport;
+		private readonly RunCommand _command;
+		private readonly LogResponse _writer;
+
+		public RunTests()
+		{
+			_writer = new LogResponse();
+
+			_transport = Substitute.For<ITransportModel>();
+
+			_transport
+				.GetProjects("Primary")
+				.Returns(new[] { TestProject, OtherProject });
+
+			_transport
+				.GetProjects("Secondary")
+				.Returns(new[] { OtherProject });
+
+			_command = new RunCommand(_writer, _transport);
+		}
+
+		[Fact]
+		public void When_no_project_name_is_supplied()
+		{
+			var input = new RunInputModel();
+
+			_command.Execute(input);
+
+			_transport.DidNotReceive().TriggerProject(Arg.Any<string>(), Arg.Any<string>());
+		}
+
+		[Fact]
+		public void When_just_a_project_name_is_supplied_and_it_is_unique()
+		{
+			var input = new RunInputModel { Project = "Test Project" };
+
+			_command.Execute(input);
+
+			_transport.Received().TriggerProject("Primary", "Test Project");
+		}
+
+		[Fact]
+		public void When_just_a_project_name_is_supplied_and_it_is_not_unique()
+		{
+			var input = new RunInputModel { Project = "Other Project" };
+
+			_command.Execute(input);
+
+			_transport.DidNotReceive().TriggerProject(Arg.Any<string>(), Arg.Any<string>());
+
+			_writer.Log.ShouldEqual(new[]
+			{
+				"Error, Ambiguous Project Name:",
+				"Did you mean:",
+				"    Primary/Test Project",
+				"    Secondary/Test Project",
+				"",
+			});
+
+		}
+
+		[Fact]
+		public void When_a_project_name_is_supplied_and_it_does_not_exist()
+		{
+			var input = new RunInputModel { Project = "Some Project" };
+
+			_command.Execute(input);
+
+			_transport.DidNotReceive().TriggerProject(Arg.Any<string>(), Arg.Any<string>());
+
+			_writer.Log.ShouldEqual(new[]
+			{
+				"Error, Unable to find project 'Some Project'.",
+				""
+			});
+		}
+
+		[Fact]
+		public void When_a_project_and_server_name_is_supplied_and_it_doesnt_exist()
+		{
+			var input = new RunInputModel { Project = "Primary/Some Project" };
+
+			_command.Execute(input);
+
+			_transport.DidNotReceive().TriggerProject(Arg.Any<string>(), Arg.Any<string>());
+
+			_writer.Log.ShouldEqual(new[]
+			{
+				"Error, Unable to find project 'Primary/Some Project'.",
+				""
+			});
+		}
+
+		[Fact]
+		public void When_a_project_and_server_name_is_supplied_and_it_exists()
+		{
+			var input = new RunInputModel { Project = "Primary/Test Project" };
+
+			_command.Execute(input);
+
+			_transport.Received().TriggerProject("Primary", "Test Project");
+		}
+	}
+}
