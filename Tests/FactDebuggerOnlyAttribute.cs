9d57e9f7da3bbde478b9055f48a3f524096213a2
andy <andy@andydote.co.uk>
2014-07-07 19:44:52 +0100

    DebuggerOnlyAttribute: rename to factDebuggerOnlyAttribute

--- Tests/FactDebuggerOnlyAttribute.cs
+++ Tests/FactDebuggerOnlyAttribute.cs
@@ -3,7 +3,7 @@ using Xunit;
 
 namespace Tests
 {
-	public class DebuggerOnlyAttribute : FactAttribute
+	public class FactDebuggerOnlyAttribute : FactAttribute
 	{
 		public override string Skip
 		{
