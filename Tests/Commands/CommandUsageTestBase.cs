aee6bc900ed42706063b176b6173a7c80572b565
andy <andy@andydote.co.uk>
2014-07-09 20:24:56 +0100

    commandUsageTestBase: adjust test base structure to fit new usages

--- Tests/Commands/CommandUsageTestBase.cs
+++ Tests/Commands/CommandUsageTestBase.cs
@@ -9,45 +9,27 @@ namespace Tests.Commands
 {
 	public abstract class CommandUsageTestBase<TCommand> where TCommand : IFubuCommand
 	{
-		private readonly List<string[]> _succeedingInput;
-		private readonly List<string[]> _failingInput;
 		private readonly CommandFactory _factory;
 
 		protected CommandUsageTestBase()
 		{
-			_succeedingInput = new List<string[]>();
-			_failingInput = new List<string[]>();
-
 			_factory = new CommandFactoryBuilder(new Container(new CruiseRegistry())).Build();
 		}
 
 		protected void Succeeds(params string[] input)
 		{
-			_succeedingInput.Add(input);
+			_factory
+				.BuildRun(input)
+				.Command
+				.ShouldBeType<TCommand>();
 		}
 
 		protected void Fails(params string[] input)
 		{
-			_failingInput.Add(input);
-		}
-
-		[Fact]
-		public void All_succeeding_usages_run_successfully()
-		{
-			_succeedingInput.ForEach(command => _factory
-				.BuildRun(command)
+			_factory
+				.BuildRun(input)
 				.Command
-				.ShouldBeType<TCommand>());
-		}
-
-		[Fact]
-		public void All_failing_usages_fail()
-		{
-			_failingInput.ForEach(command => _factory
-				.BuildRun(command)
-				.Command
-				.ShouldBeType<HelpCommand>());
-
+				.ShouldBeType<HelpCommand>();
 		}
 	}
 }
