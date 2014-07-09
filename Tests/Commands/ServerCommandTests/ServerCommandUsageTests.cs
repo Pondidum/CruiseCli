e6b5fad9ce6e5779afd23c083bd949cd200b8162
andy <andy@andydote.co.uk>
2014-07-09 20:15:59 +0100

    serverCommandUsageTests: split each call into separate tests for better output

--- Tests/Commands/ServerCommandTests/ServerCommandUsageTests.cs
+++ Tests/Commands/ServerCommandTests/ServerCommandUsageTests.cs
@@ -1,21 +1,62 @@
 ﻿using Cruise.Commands.Server;
+using Xunit;
 
 namespace Tests.Commands.ServerCommandTests
 {
 	public class ServerCommandUsageTests : CommandUsageTestBase<ServerCommand>
 	{
-		public ServerCommandUsageTests()
+		[Fact]
+		public void Calling_with_no_args_succeeds()
 		{
 			Succeeds("server");
+		}
+
+		[Fact]
+		public void Calling_with_list_succeeds()
+		{
 			Succeeds("server", "list");
+		}
+
+		[Fact]
+		public void Calling_with_add_name_and_url_succeeds()
+		{
 			Succeeds("server", "--add", "local", "http://localhost:21234/CruiseManager.rem");
+		}
+
+		[Fact]
+		public void Calling_with_remove_and_name_succeeds()
+		{
 			Succeeds("server", "--remove", "local");
+		}
 
+
+		[Fact]
+		public void Calling_with_add_and_no_args_fails()
+		{
 			Fails("server", "--add");
-			Fails("server", "--add", "local");
-			Fails("server", "--add", "http://localhost:21234/CruiseManager.rem");
+		}
 
+		[Fact]
+		public void Calling_with_add_and_only_name_fails()
+		{
+			Fails("server", "--add", "local");
+		}
+
+		[Fact]
+		public void Calling_with_add_and_only_url_fails()
+		{
+			Fails("server", "--add", "http://localhost:21234/CruiseManager.rem");
+		}
+
+		[Fact]
+		public void Calling_with_remove_and_no_args_fails()
+		{
 			Fails("server", "--remove");
+		}
+
+		[Fact]
+		public void Calling_with_remove_name_and_url_fails()
+		{
 			Fails("server", "--remove", "local", "http://localhost:21234/CruiseManager.rem");
 		}
 	}
