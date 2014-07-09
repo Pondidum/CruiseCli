25e495bc838d44f8bc8f6db53ebb537d3a491a56
andy <andy@andydote.co.uk>
2014-07-09 13:00:26 +0100

    tests: add an inMemoryConfigStore for testing with

---
+++ Tests/InMemoryConfigStore.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using Cruise.Infrastructure;
+
+namespace Tests
+{
+	public class InMemoryConfigStore : IConfigStore
+	{
+		private string _contents;
+
+		public void Write(Stream contents)
+		{
+			using (var sr = new StreamReader(contents))
+			{
+				_contents = sr.ReadToEnd();
+			}
+		}
+
+		public Stream Read()
+		{
+			var ms = new MemoryStream();
+			var writer = new StreamWriter(ms);
+
+			writer.Write(_contents);
+
+			return ms;
+		}
+	}
+}
