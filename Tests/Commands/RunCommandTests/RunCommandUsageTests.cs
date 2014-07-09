d97603cb484b9ea1eee507b048c94be543312d1e
andy <andy@andydote.co.uk>
2014-07-09 20:20:02 +0100

    runCommandUsageTests: split each call into separate tests for better output

--- Tests/Commands/RunCommandTests/RunCommandUsageTests.cs
+++ Tests/Commands/RunCommandTests/RunCommandUsageTests.cs
@@ -1,14 +1,25 @@
 ﻿using Cruise.Commands.Run;
+using Xunit;
 
 namespace Tests.Commands.RunCommandTests
 {
 	public class RunCommandUsageTests : CommandUsageTestBase<RunCommand>
 	{
-		public RunCommandUsageTests()
+		[Fact]
+		public void When_calling_with_project_name_succeeds()
 		{
 			Succeeds("run", "development");
-			Succeeds("run", "primary/development");
+		}
 
+		[Fact]
+		public void When_calling_with_a_server_and_project_name_succeeds()
+		{
+			Succeeds("run", "primary/development");
+		}
+
+		[Fact]
+		public void Calling_with_no_args_fails()
+		{
 			Fails("run");
 		}
 	}
