436d454295a8840562c494fed9c00161f9c71cd8
andy <andy@andydote.co.uk>
2014-07-09 20:24:24 +0100

    statusCommandUsageTests: split each call into separate tests for better output

--- Tests/Commands/StatusCommandTests/StatusCommandUsageTests.cs
+++ Tests/Commands/StatusCommandTests/StatusCommandUsageTests.cs
@@ -1,14 +1,27 @@
 ﻿using Cruise.Commands.Status;
+using Xunit;
 
 namespace Tests.Commands.StatusCommandTests
 {
 	public class StatusCommandUsageTests : CommandUsageTestBase<StatusCommand>
 	{
-		public StatusCommandUsageTests()
+		[Fact]
+		public void Calling_with_no_args_succeeds()
 		{
 			Succeeds("status");
+		}
+
+		[Fact]
+		public void Calling_with_project_name_succeeds()
+		{
 			Succeeds("status", "development");
+		}
+
+		[Fact]
+		public void Calling_with_a_server_and_project_name_succeeds()
+		{
 			Succeeds("status", "primary/development");
 		}
+
 	}
 }
