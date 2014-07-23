b0baf68f0e5e5747d2d6bdefd02205d286af1bdd
andy <andy@andydote.co.uk>
2014-07-23 20:07:43 +0100

    configCommandTests: add tests for color flag

--- Tests/Commands/ConfigCommandTests/ConfigCommandUsageTests.cs
+++ Tests/Commands/ConfigCommandTests/ConfigCommandUsageTests.cs
@@ -1,9 +1,26 @@
 ﻿using Cruise.Commands.Config;
+using Xunit;
 
 namespace Tests.Commands.ConfigCommandTests
 {
 	public class ConfigCommandUsageTests : CommandUsageTestBase<ConfigCommand>
 	{
-		
+		[Fact]
+		public void When_called_with_the_color_flag_and_no_args()
+		{
+			Succeeds("config", "--color");
+		}
+
+		[Fact]
+		public void When_called_with_the_color_flag_and_a_category()
+		{
+			Succeeds("config", "--color", "warning");
+		}
+
+		[Fact]
+		public void When_called_with_the_color_flag_a_cateogry_and_value()
+		{
+			Succeeds("config", "--color", "warning", "pink");
+		}
 	}
 }
