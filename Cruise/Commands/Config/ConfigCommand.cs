8997793d419e91e83f6fd268e321cc4a0f678a65
andy <andy@andydote.co.uk>
2014-07-23 20:13:10 +0100

    configCommand: add color flag and category usage

--- Cruise/Commands/Config/ConfigCommand.cs
+++ Cruise/Commands/Config/ConfigCommand.cs
@@ -8,6 +8,10 @@ namespace Cruise.Commands.Config
 		{
 			Usage("List color configuration")
 				.ValidFlags(f => f.ColorFlag);
+
+			Usage("View color value for a category")
+				.Arguments(a => a.Category)
+				.ValidFlags(f => f.ColorFlag);
 		}
 
 		public override bool Execute(ConfigInputModel input)
--- Cruise/Commands/Config/ConfigInputModel.cs
+++ Cruise/Commands/Config/ConfigInputModel.cs
@@ -3,5 +3,6 @@ namespace Cruise.Commands.Config
 	public class ConfigInputModel
 	{
 		public bool ColorFlag { get; set; }
+		public string Category { get; set; }
 	}
 }
