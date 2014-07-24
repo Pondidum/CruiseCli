edbfb4dcd09b870486950f72b06b832fea7e4a76
andy <andy@andydote.co.uk>
2014-07-24 21:13:44 +0100

    configCOmmand: add view for colorListViewModel

---
+++ Cruise/Commands/Config/ConfigColorListView.cs
@@ -0,0 +1,12 @@
+﻿using Cruise.Infrastructure.ViewEngine;
+
+namespace Cruise.Commands.Config
+{
+	public class ConfigColorListView : View<ConfigColorListViewModel>
+	{
+		public override void Render(ConfigColorListViewModel model)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
